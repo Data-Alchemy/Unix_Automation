09a93c3f5fc3cbf8da8b038cfc28eebdb80df4ab
TheAlchemist <62193858+Data-Alchemy@users.noreply.github.com>
2021-12-21 22:06:07 -0800

    Create WSL_Config.sh

---
+++ WSL_Config.sh
@@ -0,0 +1,5 @@
+sudo apt update && upgrade
+sudo apt install python3 python3-pip ipython3
+apt install python3.8-venv
+mkdir Poetry_Projects
+curl -sSL https://install.python-poetry.org | python3 - --git https://github.com/python-poetry/poetry.git@master
